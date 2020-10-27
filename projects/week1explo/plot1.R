eb5b8eae140b7a91a6fbe2d88859d745f82b61eb
Adnan18Ansari <55757910+Adnan18Ansari@users.noreply.github.com>
2020-10-27 08:19:23 +0530

    Update plot1.R

--- projects/week1explo/plot1.R
+++ projects/week1explo/plot1.R
@@ -2,23 +2,23 @@ library("data.table")
 
 # setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
-#Reads in data from file then subsets data for specified dates
+
 pow <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?"
 )
 
-# Prevents histogram from printing in scientific notation
+
 pow[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-# Change Date Column to Date Type
+
 pow[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
-# Filter Dates for 2007-02-01 and 2007-02-02
+
 pow <- pow[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 png("plot1.png", width=480, height=480)
 
-## Plot 1
+
 hist(pow[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
