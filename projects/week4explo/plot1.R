f43c1f7146f923412e52d1cfbfb57670d32ed766
Adnan18Ansari <55757910+Adnan18Ansari@users.noreply.github.com>
2020-10-27 08:46:04 +0530

    Update plot1.R

--- projects/week4explo/plot1.R
+++ projects/week4explo/plot1.R
@@ -7,7 +7,7 @@ library("data.table")
 SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
 NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
-# Prevents histogram from printing in scientific notation
+
 NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
 
 totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
